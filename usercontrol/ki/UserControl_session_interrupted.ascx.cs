cf7708b34e9305cce69e73b8b614eeb658d2fb8c
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2020-05-22 18:46:39 +0000

    Added code to inject a custom HTTP header for the partial postback error handler to check, when a session is unexpectedly interrupted.

--- usercontrol/ki/UserControl_session_interrupted.ascx.cs
+++ usercontrol/ki/UserControl_session_interrupted.ascx.cs
@@ -1,29 +1,28 @@
-using System;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.HtmlControls;
-using System.Web.UI.WebControls;
-
 namespace UserControl_session_interrupted
-{
-    public partial class TWebUserControl_session_interrupted: ki_web_ui.usercontrol_class
+  {
+  public partial class TWebUserControl_session_interrupted: ki_web_ui.usercontrol_class
     {
-        protected override void OnInit(System.EventArgs e)
-        {
-            // Required for Designer support
-            InitializeComponent();
-            base.OnInit(e);
 
-        }
+    // / <summary>
+    // / Required method for Designer support -- do not modify
+    // / the contents of this method with the code editor.
+    // / </summary>
+    private void InitializeComponent()
+      {
+      }
 
-        // / <summary>
-        // / Required method for Designer support -- do not modify
-        // / the contents of this method with the code editor.
-        // / </summary>
-        private void InitializeComponent()
-        {
-        }
+    protected override void OnInit(System.EventArgs e)
+      {
+      // Required for Designer support
+      InitializeComponent();
+      base.OnInit(e);
+      }
+
+    protected void Page_Load(object sender, System.EventArgs e)
+      {
+      Response.AppendHeader(name:CUSTOM_RESPONSE_HEADER_NAME,value:SESSION_INTERRUPTED_HEADER_VALUE);
+      }
 
     } // end TWebUserControl_session_interrupted
 
-}
+  }
